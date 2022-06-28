6b458d0163b64fbf88307a1005a150ec75e65a54
Brandon They <theyb5220@gmail.com>
2022-06-28 18:48:22 -0500

    Added daysUntilWeekend functionality

--- 03-javascript-precourse/02-Control-Flow-and-Types/Challenge/script.js
+++ 03-javascript-precourse/02-Control-Flow-and-Types/Challenge/script.js
@@ -22,3 +22,33 @@ function findGreatestNumber(num1, num2, num3) {
 
     return 0;
 }
+
+function daysUntilWeekend(day) {
+    let lowercaseDay = day.toLowerCase();
+    switch(lowercaseDay){
+        case "sunday":
+            console.log("It is the weekend!");
+            break;
+        case "monday":
+            console.log("Only 4 more days until the weekend.");
+            break;
+        case "tuesday":
+            console.log("Only 3 more days until the weekend.");
+            break;
+        case "wednesday":
+            console.log("Only 2 more days until the weekend.");
+            break;
+        case "thursday":
+            console.log("Only 1 more days until the weekend.");
+            break;
+        case "friday":
+            console.log("The last day until the weekend.");
+            break;
+        case "saturday":
+            console.log("It is the weekend!");
+            break;
+        default:
+            console.log("Sorry we didn't recognize that day.");
+            break;
+    }
+}
