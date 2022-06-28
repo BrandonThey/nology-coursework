cfdd6d26f84c1120a124b2c08cbaf1918db30393
Brandon They <theyb5220@gmail.com>
2022-06-27 20:38:24 -0500

    Added the calculate grade functionality

--- 03-javascript-precourse/02-Control-Flow-and-Types/script.js
+++ 03-javascript-precourse/02-Control-Flow-and-Types/script.js
@@ -1,1 +1,24 @@
 //javascript file for the second javascript code alog in 03-Javascript
+function handleCalculateGrade(event){
+    const grade = event.target.value;
+    let percentage;
+    switch(grade){
+        case "A":
+            percentage = "90-100%";
+            break;
+        case "B":
+            percentage = "75-90%";
+            break;
+        case "C":
+            percentage = "50-75";
+            break;
+        case "D":
+            percentage = "25-50%";
+            break;
+        case "E":
+            percentage = "0-25%";
+            break;
+    }
+
+    alert("The percentage was " + percentage);
+}
