a35283acca3696a30e807350bf76ff9e6bde386d
Brandon They <theyb5220@gmail.com>
2022-06-28 20:33:22 -0500

    Added a function and finished debugging

--- 03-javascript-precourse/03-Naming-Conventions/script.js
+++ 03-javascript-precourse/03-Naming-Conventions/script.js
@@ -1,18 +1,21 @@
 //javascript file for the third javascript code along in 03-Javascript
-const currentHeadline = "News in the world today";
+let currentHeadline = "News in the world today";
 const currentArticle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 const hasPassedTest = true;
-const amoungOfWords = 957;
+const amountOfWords = 957;
 
-if(headline.length > 20){
-    headline = headline +"..."
+if(currentHeadline.length > 20){
+    currentHeadline = currentHeadline + "...";
 }
 
 console.log("Nolofy Post 2021");
-console.log(`Headling: ${headline}`);
-console.log(article);
+console.log(`Headling: ${currentHeadline}`);
+console.log(currentArticle);
 
-function getPost() {
-    
+getWordAndCharCount(amountOfWords, currentArticle);
+
+function getWordAndCharCount(amountOfWords, currentArticle) {
+    console.log("The amount of words are: " + amountOfWords)
+    console.log("The amount of characters are: " + currentArticle.length)
 }
