9a851ffe9ebd148987598662b6aa3c6f86aeddad
Brandon They <theyb5220@gmail.com>
2022-06-28 20:19:22 -0500

    Added elements from naming conventions tutorial

--- 03-javascript-precourse/03-Naming-Conventions/script.js
+++ 03-javascript-precourse/03-Naming-Conventions/script.js
@@ -2,6 +2,9 @@
 const currentHeadline = "News in the world today";
 const currentArticle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+const hasPassedTest = true;
+const amoungOfWords = 957;
+
 if(headline.length > 20){
     headline = headline +"..."
 }
@@ -9,3 +12,7 @@ if(headline.length > 20){
 console.log("Nolofy Post 2021");
 console.log(`Headling: ${headline}`);
 console.log(article);
+
+function getPost() {
+    
+}
