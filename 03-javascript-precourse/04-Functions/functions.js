3d3ce7ad7c69c4b25741c00bb9a656ea4f6d6bf9
Brandon They <theyb5220@gmail.com>
2022-06-29 19:53:13 -0500

    Added functions to calculate the perimeter of a square, area and circumference of a circle

--- 03-javascript-precourse/04-Functions/functions.js
+++ 03-javascript-precourse/04-Functions/functions.js
@@ -5,8 +5,20 @@ const calculatePerimeter = (width, height) =>{
     return width + width + height + height;
 }
 
-console.log(calculatePerimeter(1, 1));
+//testing the function console.log(calculatePerimeter(1, 1));
 
 // 2. Write a function to find the area of a circle when given a radius, print the area to the console
+const calculateArea = (radius) => {
+    const area = Math.PI * (radius * radius);
+    return area;
+}
+
+//testing the function console.log(calculateArea(2));
 
 // 3. Do the same to find the circumference of a circle
+const calculateCircumference = (radius) => {
+    const circumference = 2 * Math.PI * radius;
+    return circumference;
+}
+
+//testing the function console.log(calculateCircumference(3));
