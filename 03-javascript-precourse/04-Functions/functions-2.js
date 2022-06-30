0dfc1cf33a67a7de9488cba46dacb3a7a7490b84
Brandon They <theyb5220@gmail.com>
2022-06-29 20:14:14 -0500

    Created calculate supply function and added its functionality

--- 03-javascript-precourse/04-Functions/functions-2.js
+++ 03-javascript-precourse/04-Functions/functions-2.js
@@ -11,3 +11,22 @@ const getDescription = (numberOfChildren, partnersName, geographicLocation, jobT
 getDescription(4, "Sandy", "Texas", "programmer");
 getDescription(10, "John", "Washington State", "sales representative");
 getDescription(1, "Lisa", "Los Angeles, California", "telemarketer");
+
+// Lifetime supply of Snickers
+
+// Write a function named calculateSupply that:
+// takes 2 arguments: age, amount per day
+// calculates the amount consumed for the rest of your life (based on a constant max age)
+// outputs the result to the screen like so: "You will need X to last until the ripe old age of Y"
+// Call that function 3 times passing different values
+// bonus: accept floating point values for amount per day and round the results to a round number
+
+const calculateSupply = (age, amountPerDay) => {
+    const numberOfDays = age * 365;
+    let amountOfSnickers = numberOfDays * amountPerDay;
+    amountOfSnickers = Math.round(amountOfSnickers);
+
+    const message = `You will need ${amountOfSnickers} to last until the ripe old age of ${age}`;
+    console.log(message);
+}
+
