0444f8d7e6a8b5a1b0e270eabe6f95189cfb3f07
Brandon They <theyb5220@gmail.com>
2022-06-29 19:53:44 -0500

    oops forgot to track the files

---
+++ 03-javascript-precourse/04-Functions/dist/functions.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Challenge
+// 1. write a function to find the parameter of a rectangle
+var calculatePerimeter = function calculatePerimeter(width, height) {
+  return width + width + height + height;
+};
+
+console.log(calculatePerimeter(1, 1)); // 2. Write a function to find the area of a circle when given a radius, print the area to the console
+// 3. Do the same to find the circumference of a circle
