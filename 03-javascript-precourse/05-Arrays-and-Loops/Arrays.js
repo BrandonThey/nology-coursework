4a060181055b3b3d9c7f47f84a37fe5a50804ec6
Brandon They <theyb5220@gmail.com>
2022-06-30 18:56:36 -0500

    Finished code along

--- 03-javascript-precourse/05-Arrays-and-Loops/Arrays.js
+++ 03-javascript-precourse/05-Arrays-and-Loops/Arrays.js
@@ -1,3 +1,13 @@
 // 1. Create a coaches array
+const coaches = ["Andy", "Sam", "Bex"];
+console.log(coaches[1]);
 
 // 2. Create an array of pets
+const pets = ["Fido", "Rusty", "Jack"];
+console.log(pets[2]);
+
+const fibNumbers= [1,1,2,3,5,8,13];
+console.log(fibNumbers);
+
+const trueFalse = [true, true, false, false];
+console.log(trueFalse);
