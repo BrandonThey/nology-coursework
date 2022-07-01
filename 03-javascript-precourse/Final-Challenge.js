fbc324e92f85aee29458320d249b0de647cf7dc6
Brandon They <theyb5220@gmail.com>
2022-06-30 19:19:06 -0500

    Createad, tested and debugged the convertDaysToAge function

--- 03-javascript-precourse/Final-Challenge.js
+++ 03-javascript-precourse/Final-Challenge.js
@@ -10,3 +10,13 @@ const isNumberPositive = (numberToTest) => {
 // testing the first function
 console.log(isNumberPositive(-1)); // returns false
 console.log(isNumberPositive(10)); // returns true
+
+// 2. Write a function that takes a number of days and converts it into an age.
+const convertDaysToAge = (numberOfDays) => {
+    ageInYears = Math.floor(numberOfDays / 365);
+    return ageInYears;
+}
+
+// testing the convert days to age function
+console.log(convertDaysToAge(3650)); // returns 10
+console.log(convertDaysToAge(6570)); // returns 18
